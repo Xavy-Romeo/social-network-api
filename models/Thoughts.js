4fa781c58f9c3766ca7cc6bc7d0753a507e32676
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-06-19 10:41:23 -0700

    added thoughts and reaction schemas

---
+++ models/Thoughts.js
@@ -0,0 +1,72 @@
+const {Schema, model, Types} = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+// Reaction Schema
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'Please enter your reaction!'
+        },
+        username: {
+            type: String,
+            required: 'Please enter your username!'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+
+);
+
+// Thoughts Schema
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Please enter your thoughts!',
+            min: 1,
+            max: 280,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Please enter your username!'
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtual: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of reactions to the Thought
+ThoughtsSchema.virtual('reactionsCount').get(function() {
+    return this.reactions.length;
+});
+
+// create Thoughts model using the Thoughts Schema
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+// exporting Thoughts model
+module.export = Thoughts;
